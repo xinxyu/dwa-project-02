1a0605dfc6c3df354304f35ec9d832d0058a5c0c
Xin Yu <xin.yu55@gmail.com>
2016-09-17 09:28:25 -0400

    Inital commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+    require('generator.php');
+?>  
+<html>
+
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/main.css" />
+        <link rel="stylesheet" href="css/bootstrap.min.css" />
+        <link rel="stylesheet" href="css/font-awesome.min.css" />
+        <title>Password Generator</title>
+    </head>
+
+
+    <body>
+
+    <span>Generator says <?php echo $message; ?></span>
+
+    </body>
+
+<html>
