b605d251938cc6e47fd38de1e4d437b88fe5ec53
Xin Yu <xin.yu55@gmail.com>
2016-09-17 20:58:00 -0400

    Update to generator logic

--- generator.php
+++ generator.php
@@ -1,2 +1,57 @@
 <?php
-$message = "hello";
+abstract class Separator
+{
+	const space = " ";
+	const hyphen = "-";
+	const camel = "camel";
+}
+
+
+function generatePassword($dictionary, $symbols, $numWords = 4, $numNumbers = 1, $numSymbols = 1, $separator = Separator::space)
+{
+    $wordsArray = [];
+    $passwordString = "";
+    # pick random words in the dictionary
+    for ($i = 0; $i < $numWords; $i++)
+    {
+        $pick = rand(0, count($dictionary) - 1);
+        $word = $dictionary[$pick];
+        if ($i > 0 && $separator == Separator::camel)
+        {
+            $word = ucfirst($word);
+        }
+        array_push($wordsArray, $word);
+    }
+
+    # concatenate random words
+    if ($separator == Separator::camel)
+    {
+        $passwordString = implode($wordsArray, "");
+    }
+    else{
+        $passwordString = implode($wordsArray, $separator);
+    }
+
+    # add symbols
+    for($i = 0; $i < $numSymbols; $i++){
+        $passwordString .= $symbols[rand(0,count($symbols) -1 )];
+    }
+
+    # add numbers
+    for($i = 0; $i < $numNumbers; $i++){
+
+        $passwordString .= strval(rand(0,9));
+    }
+
+    return $passwordString;
+}
+
+$dictionary = file("generator_sources/google-10000-english-usa.txt");
+$symbols = file("generator_sources/symbols.txt");
+
+
+
+
+
+$password = generatePassword($dictionary, $symbols);
+
--- index.php
+++ index.php
@@ -2,7 +2,6 @@
     require('generator.php');
 ?>  
 <html>
-
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="css/main.css" />
@@ -10,12 +9,7 @@
         <link rel="stylesheet" href="css/font-awesome.min.css" />
         <title>Password Generator</title>
     </head>
-
-
     <body>
-
-    <span>Generator says <?php echo $message; ?></span>
-
+    <h1>Generator says <?echo $password; ?></h1>
     </body>
-
 <html>
